f0d288dac75cb4a21ab122de873b83aba3ba762c
Kartik150704 <108309065+Kartik150704@users.noreply.github.com>
2022-09-14 23:51:15 +0530

    fixed multiply sign glitch

--- code.c
+++ code.c
@@ -128,6 +128,10 @@ void _MUL_(llu x, llu y, llu *carry, llu *result) {
 
 BigInt Multiply(const BigInt a, const BigInt b) {
     BigInt c = new_BigInt(a->len + b->len);
+    if(a->sign + b->sign==1)
+    {
+        c->sign=0;
+    }
     llu carry;
     set_zero(c);
     for (unsigned int i = 0; i < a->len; i++) {
